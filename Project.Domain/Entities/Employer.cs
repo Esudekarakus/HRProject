be6ed59474df8c2980c940872910b5a0db91e7a9
reyhangunduz <144460500+reyhangunduz@users.noreply.github.com>
2024-03-05 10:41:21 +0300

    Employer Entity oluşturuldu.

---
+++ Project.Domain/Entities/Employer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Domain.Entities
+{
+    public class Employer:BaseEntity
+    {
+       public string LastName { get; set; }
+       
+    }
+}
