03e9d510bd72d3b913ea55aeddf229fda191715d
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2024-03-05 10:29:28 +0300

    base entity eklendi

---
+++ Project.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set;}
+        public DateTime DeletedDate
+        { get; set; }
+    }
+}
