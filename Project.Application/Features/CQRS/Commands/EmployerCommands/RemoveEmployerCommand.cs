60b1793703b480274308748cbc7a6e80e6da88d8
fatihkabalcioglu <onlykabalci@gmail.com>
2024-03-05 15:20:33 +0300

    commandlar eklendi

---
+++ Project.Application/Features/CQRS/Commands/EmployerCommands/RemoveEmployerCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Application.Features.CQRS.Commands.EmployerCommands
+{
+    public class RemoveEmployerCommand
+    {
+        public RemoveEmployerCommand(int id)
+        {
+            this.Id = id;
+        }
+        public int Id { get; set; }
+    }
+}
