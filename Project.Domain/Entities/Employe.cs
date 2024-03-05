2047b796a9bfbc9cbdb6fc6f774336f868be127d
fatihkabalcioglu <onlykabalci@gmail.com>
2024-03-05 10:56:22 +0300

    employe eklendi

---
+++ Project.Domain/Entities/Employe.cs
@@ -0,0 +1,16 @@
+﻿using Project.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Domain.Entities
+{
+    public class Employe : BaseEntity
+    {
+        public string LastName { get; set; }
+        public int Salary { get; set; }
+        public OffDay OffDay { get; set; }
+    }
+}
