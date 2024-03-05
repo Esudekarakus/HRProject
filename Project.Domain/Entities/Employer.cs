51af2f0e7f4fd8025a831cf42d3e45089a8126c5
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2024-03-05 14:01:36 +0300

    employer güncellendi

--- Project.Domain/Entities/Employer.cs
+++ Project.Domain/Entities/Employer.cs
@@ -9,6 +9,30 @@ namespace Project.Domain.Entities
     public class Employer:BaseEntity
     {
        public string LastName { get; set; }
-       
+       public string SecondName { get; set; }
+       public string SecondLastName {get; set; }
+
+       public DateTime DateOfBirth { get; set; }
+       public Status Status { get; set; }
+
+       public string ImagePath { get; set; }
+       public string Address {  get; set; }
+
+       public string Telephone { get; set; }
+       public double Salary { get; set; }
+       public string Profession {  get; set; }
+        
+       public Company? Company { get; set; }
+       public int? CompanyId { get; set; }
+
+       public string Email
+        {
+            get
+            {
+                string birthYear = DateOfBirth.Year.ToString();
+                string email=$"{LastName?.ToLower()}{birthYear}@bilgeadam.boost";
+                return email;
+            }
+        }
     }
 }
