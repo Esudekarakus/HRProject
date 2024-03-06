267d6dc032123ffa4b235da18c074e0b19bcb4d2
fatihkabalcioglu <onlykabalci@gmail.com>
2024-03-06 16:23:03 +0300

    company commandlar eklendi

---
+++ Project.Application/Features/CQRS/Commands/CompanyCommands/UpdateCompanyCommand.cs
@@ -0,0 +1,18 @@
+﻿using Project.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Application.Features.CQRS.Commands.CompanyCommands
+{
+    public class UpdateCompanyCommand
+    {
+        public int Id { get; set; }
+        public DateTime FoundationDate { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public int VatNumber { get; set; }
+    }
+}
