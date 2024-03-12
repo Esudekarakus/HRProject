cf32674e8e1bd67f712f230205b5708e246b9e92
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2024-03-12 17:16:01 +0300

    d

--- Project.Application/Features/CQRS/Commands/EmployeeCommands/CreateEmployeeCommand.cs
+++ Project.Application/Features/CQRS/Commands/EmployeeCommands/CreateEmployeeCommand.cs
@@ -12,7 +12,7 @@ namespace Project.Application.Features.CQRS.Commands.EmployeeCommands
     public class CreateEmployeeCommand
     {
         public string Name { get; set; }
-        public string FirstName { get; set; }
+        
         public string MiddleName { get; set; }
         public string LastName { get; set; }
         public string SecondLastName { get; set; }
