792e86e09afa7ae7d5b469a51d4359a2236305d1
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2024-03-21 12:43:43 +0300

    usercontroller eklendi

---
+++ Project.WebApi/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Project.Domain.Entities;
+
+namespace Project.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public UserController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetUserById(string id)
+        {
+            var value = await userManager.FindByIdAsync(id);
+            return Ok(value);
+
+        }
+    }
+}
